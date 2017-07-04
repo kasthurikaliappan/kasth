3be677db9c79c3d82f28b1e3b7f22fa70ab6cd25
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-07-04 12:19:55 +0530

    Create swapping.java

---
+++ swapping.java
@@ -0,0 +1,14 @@
+package kas;
+import java.util.*;
+public class Swap {
+	public static void main(String[] args) {
+		int a=10,b=20,c=30;
+		a=a+b+c;
+		b=(a-b-c);
+		c=(a-b-c);
+		a=(a-b-c);
+		System.out.println("the swap is"+(a));
+		System.out.println("the swap is"+(b));
+		System.out.println("the swap is"+(c));
+	}
+}
